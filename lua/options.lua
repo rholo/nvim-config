efd83c249f69279f32e9d5f04fa036b01f130449
rholo <hola@rholo.cl>
2023-04-30 19:22:48 -0400

    feat(lua): create init file, folder config

---
+++ lua/options.lua
@@ -0,0 +1,20 @@
+local opt = vim.opt
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tab & indentation
+opt.tabstop = 2
+opt.shiftwith = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+
