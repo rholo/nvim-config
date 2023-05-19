994fae241e5cd14295fd2d726cf4373e3dd79aed
rholo <hola@rholo.cl>
2023-05-18 22:37:29 -0400

    feat(lua): add diagnostic config

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -2,8 +2,40 @@ local status, lsp_config = pcall(require, 'lspconfig')
 if (not status) then return end
 
 -- local protocol = require('vim.lsp.protocol')
-
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- Remove diagnostic label inline
+vim.diagnostic.config({
+  virtual_text = false
+})
+
+-- Add custom icons
+local signs = { Error = "󰠭", Warn = "", Hint = "", Info = "" }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+-- Custom diagnostic message
+function PrintDiagnostics(opts, bufnr, line_nr, client_id)
+  bufnr = bufnr or 0
+  line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
+  opts = opts or {['lnum'] = line_nr}
+
+  local line_diagnostics = vim.diagnostic.get(bufnr, opts)
+  if vim.tbl_isempty(line_diagnostics) then return end
+
+  local diagnostic_message = ""
+  for i, diagnostic in ipairs(line_diagnostics) do
+    diagnostic_message = diagnostic_message .. string.format("%d: %s", i, diagnostic.message or "")
+    print(diagnostic_message)
+    if i ~= #line_diagnostics then
+      diagnostic_message = diagnostic_message .. "\n"
+    end
+  end
+  vim.api.nvim_echo({{diagnostic_message, "Normal"}}, false, {})
+end
+vim.o.updatetime = 250
+vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
